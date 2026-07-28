648251e2c54d0c70f319c99d4d4a554f33aed03d
Jakub Dmochowski <jakubson03@outlook.com>
2026-07-28 14:56:40 +0200

    Initialize Sentinel Shield project

---
+++ src/client/java/com/nythral/sentinel/client/SentinelShieldClient.java
@@ -0,0 +1,10 @@
+package com.nythral.sentinel.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SentinelShieldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
